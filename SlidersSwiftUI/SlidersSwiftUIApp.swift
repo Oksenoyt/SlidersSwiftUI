8e0fdcf452fe91c3c2bce2cc4c52bc03043fd833
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-14 00:56:35 +0400

    Initial Commit

---
+++ SlidersSwiftUI/SlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUIApp.swift
+//  SlidersSwiftUI
+//
+//  Created by Elenka on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
